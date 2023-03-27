b6dffff764591f2e0a03f929701134f70f0e65a0
Carl Halligan <carl.halligan@gmail.com>
2023-03-27 18:32:27 +0100

    Test

--- Name Convert/server.js
+++ Name Convert/server.js
@@ -4,18 +4,43 @@ const nameConverter = require('./nameConverter');
 const path = require('path');
 const NetlifyExpress = require('netlify-express');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.post('/convert', (req, res) => {
+app.post('/convert', async (req, res) => {
     const names = req.body.names;
     const nameFormat = req.body.nameFormat;
     const convertedNames = nameConverter.convertNames(names, nameFormat);
     const responseText = nameFormat === 'powerapps' ? `["${convertedNames.join('","')}"]` : convertedNames.join('; ');
-    res.send(responseText);
+
+    // Save the response to the database
+    const userId = req.user.sub;
+    const timestamp = new Date().toISOString();
+    const query = {
+        text: 'INSERT INTO conversions (user_id, response_text, timestamp) VALUES ($1, $2, $3)',
+        values: [userId, responseText, timestamp]
+    };
+    await client.query(query);
+
+    // Retrieve the user's last 10 conversions from the database
+    const query2 = {
+        text: 'SELECT response_text, timestamp FROM conversions WHERE user_id = $1 ORDER BY timestamp DESC LIMIT 10',
+        values: [userId]
+    };
+    const result = await client.query(query2);
+    const conversions = result.rows;
+
+    res.render('index', { conversions, isAuthenticated: true });
 });
 
 const netlifyExpress = NetlifyExpress({
